16dd3a477673c9b26461a2089d8a53884b83dcbd
Douglas alves dos santos <dalves.info@gmail.com>
2024-06-16 19:01:42 -0300

    Remove teste conexão equipamentos

--- sistema.py
+++ sistema.py
@@ -5,7 +5,6 @@ from email.mime import application
 from lib.menu_tarefas import abrir_taref
 from lib.menu_avancado import abrir_avanc
 from lib.menu_automacao import abrir_autom
-from lib.menu_aplicacao import abrir_aplic
 from lib.menu_wsl import abrir_wsl
 
 from variaveis.interface_config import *
@@ -21,15 +20,14 @@ from lib import *
 myfile_cp_logs = r'C:/scripts_logs'
 
 # Dados menu em lista
-mlist = ['Testes de Conexao', 'Agilizando Tarefas', 'Test de Rede', 'Testes Automatizados', 
-'Testes de Monitoracao', 'Tarefas em WSL','Web system', 'Captura de Tela', 'Sair']
+mlist = ['Testes de Conexao', 'Agilizando Tarefas', 'Test de Rede', 'Testes Automatizados', 'Tarefas em WSL','Web system', 'Captura de Tela', 'Sair']
 
 #------------------------------------------------
 #Codigo do menu principal
 
 
 while True:
-    resposta = menu([mlist[0], mlist[1], mlist[2], mlist[3], mlist[4], mlist[5], mlist[6], mlist[7], mlist[8]])
+    resposta = menu([mlist[0], mlist[1], mlist[2], mlist[3], mlist[4], mlist[5], mlist[6], mlist[7]])
     if resposta == 1:
         print('{}'.format(op1))
         os.system('cls') or None
@@ -47,20 +45,16 @@ while True:
         print('{}'.format(op4), 'Validacao Automatizada', '\n')
         abrir_autom()
     elif resposta == 5:
-        os.system('cls') or None
-        print('{}'.format(op5), 'Monitoracao da conexao', '\n')
-        abrir_aplic()
-    elif resposta == 6:
-        print('{}'.format(op6))
+        print('{}'.format(op5))
         os.system('cls') or None
         abrir_wsl()
-    elif resposta == 7:
+    elif resposta == 6:
         os.system('cls') or None
-        print('{}'.format(op8, 'Site em HTML'))
+        print('{}'.format(op7, 'Site em HTML'))
         os.startfile('aplication.html')
-    elif resposta == 8:
+    elif resposta == 7:
         gerar_print()
-    elif resposta == 9:
+    elif resposta == 8:
         funcao_sair()
     else:
         leia_opcao()
