7f69e2ec65c55575ce8f59a7d814f92d12a740c8
douglas.alves <douglas.alves@bancobmg.com.br>
2025-06-27 17:11:41 -0300

    novas consultas aws

--- infos-adicionais/consultasaws.sh
+++ infos-adicionais/consultasaws.sh
@@ -17,22 +17,62 @@ select opt in "${REGIONS[@]}"; do
     esac
 done
 
+# -------------------------------------------------------------#
+
+# Listando clusters EKS na região selecionada
 echo -e "\n🔍 Verificando clusters EKS na região $REGION..."
 aws eks list-clusters --region "$REGION" --output json | jq -r '.clusters[]?'
 
-echo -e "\n🔍 Verificando clusters MSK padrão na região $REGION..."
-aws kafka list-clusters --region "$REGION" --output json | jq -r '.ClusterInfoList[]?.ClusterName'
-
+# Listando clusters MSK na região selecionada
 echo -e "\n🔍 Verificando clusters MSK Serverless na região $REGION..."
 aws kafka list-clusters-v2 --region "$REGION" --output json | jq -r '.ClusterInfoList[]?.ClusterName'
 
+# Listando brokers do Amazon MQ na região selecionada
 echo -e "\n🔍 Verificando brokers do Amazon MQ na região $REGION..."
 aws mq list-brokers --region "$REGION" --output json | jq -r '.BrokerSummaries[]?.BrokerName'
 
+
+# -------------------------------------------------------------#
 echo
 read -p "Informe a sigla para filtrar os secrets: " SIGLA
 
+# Verificando se a sigla foi informada
 echo -e "\n🔍 Buscando secrets que contenham '$SIGLA' na região $REGION..."
+
+if [ -z "$SIGLA" ]; then
+    echo "Nenhuma sigla informada. Listando todos os secrets."
+    SIGLA=".*"  # Regex que corresponde a qualquer string
+fi
+# Listando secrets no Secrets Manager filtrados pela sigla
 aws secretsmanager list-secrets --region "$REGION" --output json | \
 jq -r --arg sigla "$SIGLA" '.SecretList[]? | select(.Name | test($sigla; "i")) | .Name'
 
+# -------------------------------------------------------------#
+
+echo
+read -p "Informe o namespace para consultar as ServiceAccounts: " NAMESPACE
+
+echo -e "\n🔍 Listando ServiceAccounts no namespace '$NAMESPACE'..."
+if [ -z "$NAMESPACE" ]; then
+    echo "Nenhum namespace informado. Usando o namespace padrão 'default'."
+    NAMESPACE="default"
+fi
+
+# Listando ServiceAccounts no namespace informado
+if [ $? -ne 0 ]; then
+    echo "Erro ao listar ServiceAccounts. Verifique se o namespace '$NAMESPACE' existe."
+else
+    echo -e "\n🔍 Verificando ServiceAccounts com secrets no namespace '$NAMESPACE'..."
+    kubectl get serviceaccounts -n "$NAMESPACE"
+fi
+
+# Listando ServiceAccounts com role-arn, se houver
+echo -e "\n🔍 Listando ServiceAccounts no namespace '$NAMESPACE' com role-arn (se houver)..."
+kubectl get serviceaccounts -n "$NAMESPACE" -o json | jq -r '
+  .items[] | 
+  {
+    name: .metadata.name,
+    roleArn: (.metadata.annotations."eks.amazonaws.com/role-arn" // "N/A")
+  } | 
+  "🔹 SA: \(.name)\n   ↳ Role ARN: \(.roleArn)\n"
+'
