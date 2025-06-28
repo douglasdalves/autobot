f51fc73479675e936b614addf8046c5b9774b72f
Doug santos <ti.douglas.santos@gmail.com>
2025-06-28 16:32:17 -0300

    otimizando o menu

--- autobot-wsl.py
+++ autobot-wsl.py
@@ -9,82 +9,21 @@ from lib_autobot.docker_comandos import fun_start_docker
 from lib_autobot.docker_comandos import fun_stop_docker
 from lib_autobot.docker_comandos import verificar_docker_running
 from lib_autobot.docker_comandos import verificar_docker_dados
+from lib_autobot.other_comands import listar_credenciais
+from lib_autobot.other_comands import listar_perfis
+from lib_autobot.other_comands import list_version
+from lib_autobot.other_comands import verificar_ambiente_e_executar_versao
 from lib_autobot.kube_comandos import dev_kube
 from lib_autobot.kube_comandos import list_helm
 
 from lib_autobot.lib_comandos import *
-
-# ------------------------------------------
-# ------------------------------------------
-
-def listar_credenciais():
-    cabecalho_sub('Listando credenciais AWS (PARCIAL)')
-    try:
-        with open(os.path.expanduser("~/.aws/credentials"), 'r') as f:
-            for line in f:
-                if line.startswith("["):
-                    print(colored(line.strip(), 'yellow'))  # Mostra apenas o nome dos perfis
-    except FileNotFoundError:
-        print(colored('Arquivo de credenciais não encontrado.', 'red'))
-
-def listar_configuracoes():
-    cabecalho_sub('Listando configurações AWS')
-    executar_comando(['cat', os.path.expanduser("~/.aws/config")], shell=True)
-
-
-def listar_perfis():
-    try:
-        listaraws = executar_comando(['aws', 'configure', 'list-profiles'])
-        
-        # Verifica se a saída contém a palavra 'running'
-        if 'Erro' or 'erro' in listaraws.stdout:
-            print(colored(f"Em desenvolvimento", 'blue'))
-            executar_comando(['aws', 'configure', 'list-profiles'])
-        else:
-            cabecalho_sub('Listando perfis configurados')
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
-
-
-
-# ------------------------------------------
-# ------------------------------------------
-
-def listar_versao(nome, comando):
-    cabecalho_cor(f"Versão {nome}")
-    executar_comando(comando)
-
-def verificar_ambiente_e_executar_versao():
-    usuario_host = comando_host()
-    
-    if usuario_host == "douglas@ACT9880":
-        acao_para_ambiente_correto()
-        listar_versao('ASRE cli', ['asre', 'version'])
-    else:
-        acao_para_ambiente_errado()
-
-
-def list_version():
-    cabecalho_sub('Listar versões instaladas')
-    
-    versoes = {
-        'Git': ['git', '--version'],
-        'Docker': ['docker', '--version'],
-        'Python': ['python3', '--version'],
-        'AWS cli': ['aws', '--version'],
-        'Kubectl': ['kubectl', 'version', '--client', '--output=yaml'],
-        'K9s': ['k9s', 'version'],
-        'Helm': ['helm', 'version']  
-    }
-    
-    for nome, comando in versoes.items():
-        listar_versao(nome, comando)
 
 
 # ------------------------------------------
 # Menu geral
 # ------------------------------------------
 
+# Função para exibir o cabeçalho do menu
 def menu():
     while True:
         cabecalho_menu("\nEscolha uma opção:\n")
---
+++ lib_autobot/other_comands.py
@@ -0,0 +1,71 @@
+# ------------------------------------------
+# Imports
+# ------------------------------------------
+
+from lib_autobot.lib_comandos import *
+
+# ------------------------------------------
+# ------------------------------------------
+
+def listar_credenciais():
+    cabecalho_sub('Listando credenciais AWS (PARCIAL)')
+    try:
+        with open(os.path.expanduser("~/.aws/credentials"), 'r') as f:
+            for line in f:
+                if line.startswith("["):
+                    print(colored(line.strip(), 'yellow'))  # Mostra apenas o nome dos perfis
+    except FileNotFoundError:
+        print(colored('Arquivo de credenciais não encontrado.', 'red'))
+
+def listar_configuracoes():
+    cabecalho_sub('Listando configurações AWS')
+    executar_comando(['cat', os.path.expanduser("~/.aws/config")], shell=True)
+
+
+def listar_perfis():
+    try:
+        listaraws = executar_comando(['aws', 'configure', 'list-profiles'])
+        
+        # Verifica se a saída contém a palavra 'running'
+        if 'Erro' or 'erro' in listaraws.stdout:
+            print(colored(f"Em desenvolvimento", 'blue'))
+            executar_comando(['aws', 'configure', 'list-profiles'])
+        else:
+            cabecalho_sub('Listando perfis configurados')
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+
+
+
+# ------------------------------------------
+# ------------------------------------------
+
+def listar_versao(nome, comando):
+    cabecalho_cor(f"Versão {nome}")
+    executar_comando(comando)
+
+def verificar_ambiente_e_executar_versao():
+    usuario_host = comando_host()
+    
+    if usuario_host == "douglas@ACT9880":
+        acao_para_ambiente_correto()
+        listar_versao('ASRE cli', ['asre', 'version'])
+    else:
+        acao_para_ambiente_errado()
+
+
+def list_version():
+    cabecalho_sub('Listar versões instaladas')
+    
+    versoes = {
+        'Git': ['git', '--version'],
+        'Docker': ['docker', '--version'],
+        'Python': ['python3', '--version'],
+        'AWS cli': ['aws', '--version'],
+        'Kubectl': ['kubectl', 'version', '--client', '--output=yaml'],
+        'K9s': ['k9s', 'version'],
+        'Helm': ['helm', 'version']  
+    }
+    
+    for nome, comando in versoes.items():
+        listar_versao(nome, comando)
