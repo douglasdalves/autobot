42eb7c26f18a9ba198339be1748ac76311600044
douglas.alves <douglas.alves@bancobmg.com.br>
2025-08-08 17:11:44 -0300

    melhorias .sh cli/helm

--- infos-adicionais/consultascli.sh
+++ infos-adicionais/consultascli.sh
@@ -9,39 +9,34 @@ validar_entrada() {
     nome_do_repo=$(echo $valor | cut -d'-' -f2)
 
     if [[ ! "$sigla" =~ ^[A-Za-z]{4}$ ]]; then
-        echo "Erro: A sigla deve ter exatamente 4 letras."
+        echo "❌ Erro: A sigla deve ter exatamente 4 letras."
         exit 1
     fi
 
     if [[ -z "$nome_do_repo" ]]; then
-        echo "Erro: O nome do repositório não pode estar vazio."
+        echo "❌ Erro: O nome do repositório não pode estar vazio."
         exit 1
     fi
 }
 
 # Função para realizar a operação
 realizar_operacao() {
-    local comando=$1
-    echo -e "\n--- Realizando operação ---\n"
-    resultado=$(eval $comando 2>&1)
-    if [[ $? -ne 0 ]]; then
-        echo "Erro ao executar '$comando'"
-        exit 1
-    fi
-
-    if [[ "$resultado" == *"não localizado"* ]]; then
-        echo "Aviso: O serviço ou aplicação '$comando' não foi localizado."
+    local comando="$1"
+    echo -e "\n ✅ Realizando operação\n"
+    # Verifica se a saída contém a mensagem esperada
+    if echo "$resultado" | grep -qi "não localizado"; then
+        echo "⚠️ Aviso: O serviço ou aplicação não foi localizado."
     elif [[ -z "$resultado" || "$resultado" == "null" ]]; then
-        echo "Aviso: O comando '$comando' não retornou dados."
+        echo "⚠️ Aviso: O comando '$comando' não retornou dados."
     else
         echo "$resultado"
     fi
-    echo
 }
+
 
 # Solicita o valor de entrada
 echo
-read -p "Insira a Sigla-NomeDoRepo: " valor
+read -p "🔍 Insira Sigla-NomeDoRepo: " valor
 
 # Valida a entrada
 validar_entrada $valor
@@ -54,4 +49,4 @@ asre_apps="asre catalogo aplicacoes list | grep $sigla"
 realizar_operacao "$asre_service"
 realizar_operacao "$asre_apps"
 
-echo "Operações concluídas com sucesso."
+echo "✅ Operações concluídas com sucesso."
--- infos-adicionais/consultashelm.sh
+++ infos-adicionais/consultashelm.sh
@@ -13,18 +13,18 @@ if [ -z "$repo_name" ]; then
 fi
 
 # Executa os comandos com o nome do repositório inserido
-echo -e "\n-- Infos no EKS --\n"
+echo -e "\n 📌 Infos no EKS\n"
 kubectl get all -A | grep "$repo_name"
 
 # Exibe o campo image de todos os pods que correspondem ao repo_name
-echo -e "\n-- List ID Image --\n"
+echo -e "\n 📌 List ID Image\n"
 kubectl get pods -A -o jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.spec.containers[*].image}{'\n'}{end}" | grep "$repo_name"
 
 # Lista a versao aplicada via release helm
-echo -e "\n-- Releases List --\n"
+echo -e "\n 📌 Releases List\n"
 helm list -A | grep "$repo_name"
 
-echo -e "\n-- Releases Removida com histórico --\n"
+echo -e "\n 📌 Releases Removida com histórico\n"
 # Procura alguma release removida com o registro
 helm list --uninstalled -A | grep "$repo_name"
 echo
